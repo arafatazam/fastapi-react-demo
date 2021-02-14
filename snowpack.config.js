c5d77faf5a439616a917ea245b09d25d05e9761a
Muhammad Arafat Azam <arafatazam17@gmail.com>
2021-02-14 16:32:19 +0600

    Initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,22 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    "frontend/public": "/",
+    "frontend/src": "/dist"
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
